96803eb34ff7caf950addeb625f13c719a79a448
Pantoja14022020 <tl419411@uaeh.edu.mx>
2024-02-09 15:20:58 -0600

    Revert "puse archivos de la api de grace williams y lo remplaze por lo que habia"
    This reverts commit c77a0558d21026a742abb47be61088185ef6868a.

--- src/routes/session.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express'); //Import enruter to define all endpoint 
-const router = Router(); //We instance object router
-const {postSession} = require('../controller/session');
-
-//Recibe las credenciales desde el front end en reactjs
-router.post('/',postSession);
-
-module.exports = router;
--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express'); //Import enruter to define all endpoint 
-const router = Router(); //We instance object router
-const {getUser,postUser,putUser} = require('../controller/user');
-
-router.get('/',getUser);
-router.post('/',postUser);
-router.put('/:id',putUser);
-
-module.exports = router;
